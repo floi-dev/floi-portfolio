05bb2e77e7b2eb97e9296fd86ae778bc0a8c50b2
Cop-er <iamalosser98@gmail.com>
2025-10-18 12:48:33 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/floi-portfolio/", // 👈 change this to your repo name
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  base: "/floi-portfolio",
 });
